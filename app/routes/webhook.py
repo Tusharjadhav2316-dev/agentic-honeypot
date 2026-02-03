7503ab19a0d911094de4df9791e7a69f5d4fefc9
Tushar <tushar.jadhav2316@gmail.com>
2026-02-03 19:48:11 +0530

    Make request body optional for GUVI tester

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from app.utils.auth import verify_api_key
+from typing import Optional, Dict
 
 router = APIRouter()
 
 @router.post("/honeypot")
-def honeypot_endpoint(payload: dict, auth=Depends(verify_api_key)):
+def honeypot_endpoint(
+    payload: Optional[Dict] = Body(default={}),
+    auth=Depends(verify_api_key)
+):
     return {
         "status": "ok",
         "message": "Honeypot API is live and secured",
