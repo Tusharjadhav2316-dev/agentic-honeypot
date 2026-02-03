b57008f79c997a7e6258a8cc4093e93cec49b7dd
Tushar <tushar.jadhav2316@gmail.com>
2026-02-03 20:08:30 +0530

    update guvi tester

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -1,20 +1,16 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Response
 from app.utils.auth import verify_api_key
 
 router = APIRouter()
 
-@router.post("/honeypot")
+@router.api_route(
+    "/honeypot",
+    methods=["GET", "POST", "HEAD", "OPTIONS"],
+)
 async def honeypot_endpoint(
-    request: Request,
     auth=Depends(verify_api_key)
 ):
-    try:
-        payload = await request.json()
-    except:
-        payload = {}
-
     return {
         "status": "ok",
-        "message": "Honeypot API is live and secured",
-        "received_payload": payload
+        "message": "Honeypot API is live and secured"
     }
